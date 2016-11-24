94aa5c52861e9b23c60a65053800aa2be21f3e56
InuNatNat <NekoNatNat@gmail.com>
2016-11-23 19:44:17 -0600

    1st commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Lulu_LoL
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lulu_lol' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'lulu_lol' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'Nature' ), 'Nature', '<a href="http://NAThompsonillustration-design.com" rel="designer">NAT</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
